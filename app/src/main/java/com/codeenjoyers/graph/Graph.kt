604cf0c69aa573ea3d706d259d3d417ebd870115
Nikodem Reszka <61233315+nikodemrx@users.noreply.github.com>
2022-10-23 02:48:39 +0200

    Added graph

---
+++ app/src/main/java/com/codeenjoyers/graph/Graph.kt
@@ -0,0 +1,78 @@
+package com.codeenjoyers.graph
+
+import java.lang.StringBuilder
+import java.util.*
+
+internal class Graph<T> {
+    //creating  an  object  of  the  Map  class  that  stores  the  edges  of  the  graph
+    private val map: MutableMap<T, MutableList<T>> = HashMap()
+
+    //the  method  adds  a  new  vertex  to  the  graph
+    fun addNewVertex(s: T) {
+        map[s] = LinkedList()
+    }
+
+    //the  method  adds  an  edge  between  source  and  destination
+    fun addNewEdge(source: T, destination: T, bidirectional: Boolean) {
+        //
+        if (!map.containsKey(source)) addNewVertex(source)
+        if (!map.containsKey(destination)) addNewVertex(destination)
+        Objects.requireNonNull(map[source])?.add(destination)
+        if (bidirectional) {
+            Objects.requireNonNull(map[destination])?.add(source)
+        }
+    }
+
+    //the  method  counts  the  number  of  vertices
+    fun countVertices() {
+        println("Total  number  of  vertices:  " + map.keys.size)
+    }
+
+    //the  method  counts  the  number  of  edges
+    fun countEdges(bidirection: Boolean) {
+        //variable  to  store  number  of  edges
+        var count = 0
+        for (v in map.keys) {
+            count += Objects.requireNonNull<List<T>>(map[v]).size
+        }
+        if (bidirection) {
+            count /= 2
+        }
+        println("Total  number  of  edges:  $count")
+    }
+
+    //checks  a  graph  has  vertex  or  not
+    fun containsVertex(s: T) {
+        if (map.containsKey(s)) {
+            println("The  graph  contains  $s  as  a  vertex.")
+        } else {
+            println("The  graph  does  not  contain  $s  as  a  vertex.")
+        }
+    }
+
+    //checks  a  graph  has  edge  or  not
+    //where  s  and  d  are  the  two  parameters  that  represent  source(vertex)  and  destination  (vertex)
+    fun containsEdge(s: T, d: T) {
+        if (Objects.requireNonNull<List<T>>(map[s]).contains(d)) {
+            println("The  graph  has  an  edge  between  $s  and  $d.")
+        } else {
+            println("There  is  no  edge  between  $s  and  $d.")
+        }
+    }
+
+    //prints  the  adjacencyS  list  of  each  vertex
+    //here  we  have  overridden  the  toString()  method  of  the  StringBuilder  class
+    override fun toString(): String {
+        val builder = StringBuilder()
+        //foreach  loop  that  iterates  over  the  keys
+        for (v in map.keys) {
+            builder.append(v.toString()).append(":  ")
+            //foreach  loop  for  getting  the  vertices
+            for (w in Objects.requireNonNull<List<T>>(map[v])) {
+                builder.append(w.toString()).append("  ")
+            }
+            builder.append("\n")
+        }
+        return builder.toString()
+    }
+}
--- app/src/main/java/com/codeenjoyers/graph/MainActivity.kt
+++ app/src/main/java/com/codeenjoyers/graph/MainActivity.kt
@@ -2,10 +2,37 @@ package com.codeenjoyers.graph
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<FloatingActionButton>(R.id.runBtn).setOnClickListener {
+            val graph = Graph<Int>()
+            //adding  edges  to  the  graph
+            graph.addNewEdge(0, 1, true)
+            graph.addNewEdge(0, 4, true)
+            graph.addNewEdge(1, 2, true)
+            graph.addNewEdge(1, 3, false)
+            graph.addNewEdge(1, 4, true)
+            graph.addNewEdge(2, 3, true)
+            graph.addNewEdge(2, 4, true)
+            graph.addNewEdge(3, 0, true)
+            graph.addNewEdge(2, 0, true)
+            //prints  the  adjacency  matrix  that  represents  the  graph
+            println("Adjacency  List  for  the  graph:\n$graph")
+            //counts  the  number  of  vertices  in  the  graph
+            graph.countVertices()
+            //counts  the  number  of  edges  in  the  graph
+            graph.countEdges(true)
+            //checks  whether  an  edge  is  present  or  not  between  the  two  specified  vertices
+            graph.containsEdge(3, 4)
+            graph.containsEdge(2, 4)
+            //checks  whether  vertex  is  present  or  not
+            graph.containsVertex(3)
+            graph.containsVertex(5)
+        }
     }
 }
